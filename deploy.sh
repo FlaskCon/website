4732e922683fe3ef11865549856fc2a98877cf7a
Abhiram R <abhi.darkness@gmail.com>
2020-05-17 18:28:23 +0530

    Moving from Temp Website to Hugo themed Website

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+HUGO_ENV=production hugo -t hugo-conference
+
+# Build the project.
+ # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# # Go To Public folder
+# cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="Rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
